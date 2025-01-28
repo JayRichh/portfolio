ba8640068aa73e053b2342444310a37f01fc0e14
Jayden Richardson <jsrich@hotmail.co.nz>
2025-01-28 20:24:15 +0000

    word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.3 }}
               >
-                Kiwi innovating in the UK
+                Kiwi developing in the UK
               </motion.p>
 
               <motion.div
